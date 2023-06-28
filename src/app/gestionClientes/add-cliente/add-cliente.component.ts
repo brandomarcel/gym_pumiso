712478e58f94fa2d93b551f8e8fbcb52b08a57cc
Brando Cevallos <brandocevallos@gmail.com>
2023-06-27 23:19:38 -0500

    peso y altura

--- src/app/gestionClientes/add-cliente/add-cliente.component.ts
+++ src/app/gestionClientes/add-cliente/add-cliente.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormBuilder, FormControl,Validators  } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { formatDate } from "@angular/common";
+import { ServiciosService } from 'app/services/servicios.service';
+
 
 @Component({
   selector: 'app-add-cliente',
@@ -7,28 +10,95 @@ import { FormGroup,FormBuilder, FormControl,Validators  } from '@angular/forms';
   styleUrls: ['./add-cliente.component.scss']
 })
 export class AddClienteComponent implements OnInit {
-  
-  cedula:any='';
-  nombres:any='';
-  apellidos:any='';
-  apodo:any='';
-  celular:any='';
-  correo:any='';
-  fecha_inicio:any='';
-  fecha_fin:any='';
-  valor:any='';
-  descuento:any='';
+  //Cliente
+  cedula: any = '';
+  nombres: any = '';
+  apellidos: any = '';
+  apodo: any = '';
+  celular: any = '';
+  correo: any = '';
+  //Membresia
+  fecha_inicio: any = '';
+  fecha_fin: any = '';
+  tipo_membresia: any = 'Mensual';
+  valor: any = '25';
+  //Peso y altura
+  peso: any = '';
+  altura: any = '';
+  imc: any = '';
+  descripcion: any = '';
+  bajarpeso: any = '';
 
-  constructor(private formBuilder:FormBuilder) { }
+
+  constructor(private servicios:ServiciosService) { }
 
   ngOnInit(): void {
+  let fecha = new Date();
+  this.fecha_inicio = formatDate((fecha), 'yyyy-MM-dd', 'en-US');
+  console.log(this.fecha_inicio)
+
+  fecha.setMonth(fecha.getMonth() + 1)
+  this.fecha_fin = formatDate(fecha, 'yyyy-MM-dd', 'en-US');
+  
 
 
   }
 
-  createCliente(){
-    console.log(this.cedula)
+  createCliente() {
+
    
+    let cliente = {
+      'cedula': this.cedula, 'nombres': this.nombres, 'apellidos': this.apellidos,
+      'apodo': this.apodo, 'celular': this.celular, 'correo': this.correo,'fecha_inicio': this.fecha_inicio,
+      'fecha_fin': this.fecha_fin, 'tipo_membresia': this.tipo_membresia, 'valor': this.valor,
+      'peso': this.peso, 'altura': this.altura
+    }
+
+    this.servicios.crearCliente(cliente).subscribe((res:any)=>{
+      console.log(res)
+      if (res.message.estado == 'Exito') {
+        this.servicios.sweetMensaje('CLIENTE REGISTRADO');
+        
+      }
+      
+    })
+
+    
+
+    console.log(cliente)
+
+  }
+  calcularIMC(){
+  let res:any;
+    if (!this.peso ) {
+      this.servicios.sweetMensaje('Ingrese el peso ');
+    }else if(!this.altura){
+      this.servicios.sweetMensaje('Ingrese la altura ');
+    }
+    res =this.servicios.calcularIMC(this.peso,this.altura);
+
+    this.imc=res.imc
+    this.descripcion = res.descripcion
+    this.bajarpeso = res.bajarpeso
+    console.log(res);
+    
+
+
+  }
+
+  cambio(){
+    console.log(this.tipo_membresia)
+    if (this.tipo_membresia == 'Mensual') {
+      this.valor=25;
+    }else if (this.tipo_membresia == 'Trimestral') {
+      this.valor=75;
+    }else if (this.tipo_membresia == 'Semestral') {
+      this.valor=150;
+    }else if (this.tipo_membresia == 'Anual') {
+      this.valor=300;
+    }else if (this.tipo_membresia == 'Otro') {
+      this.valor='';
+    }
   }
 
 }
--- src/app/services/servicios.service.ts
+++ src/app/services/servicios.service.ts
@@ -1,22 +1,106 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'environments/environment';
-
+import Swal from 'sweetalert2';
 @Injectable({
   providedIn: 'root'
 })
 export class ServiciosService {
-apiUrl=environment.apiUrl;
-  constructor(private httpClient:HttpClient) { }
+  apiUrl = environment.apiUrl;
+  constructor(private httpClient: HttpClient) { }
 
   token = new HttpHeaders({
     'Content-Type': 'application/json',
   })
 
-  getClientes(){
+  getClientes() {
     let url = this.apiUrl + 'api/method/olimpusgym.clientes.doctype.cliente.cliente.getClientes'
- 
-    return this.httpClient.get( url, { headers: this.token,responseType: 'json' });
+
+    return this.httpClient.get(url, { headers: this.token, responseType: 'json' });
   }
+  crearCliente(datos) {
+    let url = this.apiUrl + 'api/method/olimpusgym.clientes.doctype.cliente.cliente.crearCliente'
+    let dato = { datos: datos }
+    return this.httpClient.post(url, dato, { headers: this.token, responseType: 'json' });
+  }
+
+
+
+  calcularIMC(peso: number, altura: number): any {
+
+    const imc = peso / (altura * altura);
+
+    console.log(imc)
+    let descripcion = '';
+    let bajarpeso: any;
+    let calculo: any = '';
+
+    calculo = (25 * (altura * altura))
+    console.log(calculo)
+    bajarpeso = (peso - calculo).toFixed(2)
+
+    console.log(bajarpeso)
+    if (imc <= 18.5) {
+      descripcion = 'Bajo peso'
+    } else if ((imc > 18.5 && imc <= 24.9)) {
+      descripcion = 'Adecuado'
+    }
+    else if ((imc >= 25 && imc <= 29.9)) {
+      descripcion = 'Sobrepeso'
+    } else if ((imc >= 30 && imc <= 34.9)) {
+      descripcion = 'Obesidad grado 1'
+    } else if ((imc >= 35 && imc <= 39.9)) {
+      descripcion = 'Obesidad grado 2'
+    } else if (imc >= 40) {
+      descripcion = 'Obesidad grado 2(Alerta)'
+    }
+
+    return {
+      'imc': imc.toFixed(2),
+      'descripcion': descripcion,
+      'bajarpeso': bajarpeso+'kg'
+    };
+  }
+
+  sweetMensaje(mensaje) {
+    Swal.fire({
+
+      icon: 'success',
+      title: mensaje,
+      showConfirmButton: false,
+      timer: 1500
+    })
+  }
+
+  async sweetMensajePromesa(titulo, icono): Promise<any> {
+    return new Promise(async (resolve) => {
+      Swal.fire({
+        title: titulo,
+        heightAuto: false,
+        icon: icono,
+        confirmButtonText: 'Ok',
+        //showDenyButton: true,
+        //denyButtonText: `Don't save`,
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          resolve('ok')
+
+        } /* else if (result.isDenied) {
+        resolve('cancelar')
+      } */
+      })
+    })
+
+  }
+
+
+
+
+
+
+
+
+
 
 }
